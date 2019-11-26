b396b5c6d3595d7d603d4ccda1546b0309460039
20143254 이태용 <pastelhawk@gmail.com>
2019-11-26 21:35:53 +0900

    9번 문제 풀이

--- num9/num9_client.c
+++ num9/num9_client.c
@@ -1,33 +1,57 @@
-/* sendpmq.c */
-/* POSIX message queue example */ #include <sys/types.h>
+#include <sys/types.h>
 #include <mqueue.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFSIZE 16
+#include <unistd.h>
+
+#define BUFSIZE 32
 #define QNAME "/my_queue"
 #define PRIORITY 1
 
-char send_data[] = "Hello, world";
 
 int main() {
   mqd_t qd;
   struct mq_attr q_attr;
-
+  char send_data[BUFSIZE];
+  int status;
+  pid_t pid;
+    
   q_attr.mq_maxmsg = 10; /* max message number in queue */
   q_attr.mq_msgsize = BUFSIZE; /* max message size */
 
-  if ((qd = mq_open(QNAME, O_CREAT | O_RDWR, 0600, &q_attr)) == -1) {
-    perror ("mq_open failed");
-    exit (1);
-  }
 
-  if (mq_send(qd, send_data, strlen(send_data), PRIORITY) == -1) {
-    perror ("mq_send failed");
-    exit (1); }
+  while(1){
+    if ((pid = fork()) == 0){
 
-  if (mq_close(qd) == -1) {
-    perror ("mq_close failed");
-    exit (1);
+      memset(send_data, 0, BUFSIZE);
+      printf("Input > ");
+      scanf("%[^\n]", &send_data);
+  
+      if ((qd = mq_open(QNAME, O_CREAT | O_RDWR, 0600, &q_attr)) == -1) {
+	perror ("mq_open failed");
+	exit (1);
+      }
+
+      if (mq_send(qd, send_data, strlen(send_data), PRIORITY) == -1) {
+	perror ("mq_send failed");
+	exit (1);
+      }
+
+      if (mq_close(qd) == -1) {
+	perror ("mq_close failed");
+	exit (1);
+      }
+      exit(0);
+    }else if(pid > 0){
+      pid = wait(&status);
+      sleep(1);
+    }
+    else{
+      perror("fork failed");
+      exit(1);
+    }
   }
 }
+
+
--- num9/num9_server.c
+++ num9/num9_server.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFSIZE 16
+#define BUFSIZE 32
 #define QNAME "/my_queue"
 #define PRIORITY 1
 
@@ -13,42 +13,61 @@ char recv_data[BUFSIZE];
 int main() {
 
   mqd_t qd;
-
+  pid_t pid;
+  int status;
   struct mq_attr q_attr, old_q_attr; int prio;
 
   char buf[BUFSIZE];
   q_attr.mq_maxmsg = 10; /* max message number in queue */
   q_attr.mq_msgsize = BUFSIZE; /* max message size */
 
-  if ((qd = mq_open(QNAME, O_RDWR | O_NONBLOCK, 0600, NULL)) == -1) {
-    perror ("mq_open failed");
-    exit (1); }
 
-  perror ("mq_setattr failed");
-  q_attr.mq_flags = 0; /* release O_NONBLOCK */
-  if (mq_setattr(qd, &q_attr, NULL)) {
-    exit (1);
-  }
-  if (mq_getattr(qd, &old_q_attr)) {
-    perror ("mq_getattr failed");
-    exit (1);
-  }
+  while(1){
+    if ((pid = fork()) == 0){
+
+      if ((qd = mq_open(QNAME, O_RDWR | O_NONBLOCK, 0600, NULL)) == -1) {
+	perror ("mq_open failed");
+	exit (1);
+      }
+      q_attr.mq_flags = 0; /* release O_NONBLOCK */
+
+      if (mq_setattr(qd, &q_attr, NULL)) {
+	exit (1);
+      }
+
+      if (mq_getattr(qd, &old_q_attr)) {
+	perror ("mq_getattr failed");
+	exit (1);
+      }
   
-  if (!(old_q_attr.mq_flags & O_NONBLOCK))
-    printf("O_NONBLOCK not set\n");
+      if (!(old_q_attr.mq_flags & O_NONBLOCK));
 
-  if (mq_receive(qd, recv_data, BUFSIZE, &prio) == -1) {
-    perror ("mq_send failed");
-    exit (1); }
-  printf ("received from message queue : %s, prio : %d\n", recv_data, prio);
+      if (mq_receive(qd, recv_data, BUFSIZE, &prio) == -1) {
+	perror ("mq_send failed");
+	exit (1);
+      }
 
-  if (mq_close(qd) == -1) {
-    perror ("mq_close failed");
-    exit (1);
-  }
+      printf ("User Say : %s! \n", recv_data);
+
+      if (mq_close(qd) == -1) {
+	pid = wait(&status);
+	perror ("mq_close failed");
+	exit (1);
+      }
   
-  if (mq_unlink(QNAME) == -1) {
-    perror ("mq_unlink failed");
-    exit (1);
+      if (mq_unlink(QNAME) == -1) {
+	perror ("mq_unlink failed");
+	exit (1);
+      }
+      exit(0);
+    }else if(pid > 0){
+      pid = wait(&status);
+      sleep(3);
+    }
+    else{
+      perror("fork failed");
+      exit(1);
+    }
   }
 }
+
